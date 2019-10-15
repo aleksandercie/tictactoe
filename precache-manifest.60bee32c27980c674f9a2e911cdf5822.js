72bd7637c874b2761fb22d1fd69d5c91a977ad7a
Aleksander <aleksander@MacBook-Air-Aleksander.local>
2019-10-15 10:28:28 +0200

    Updates

---
+++ precache-manifest.60bee32c27980c674f9a2e911cdf5822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2290f56f2dcf137c88586a8c8c4c754",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "f3ab421edcfe51dc39be",
+    "url": "/tictactoe/static/css/main.7b98c564.chunk.css"
+  },
+  {
+    "revision": "d92d8392906a3cbb6c6b",
+    "url": "/tictactoe/static/js/2.0c89014c.chunk.js"
+  },
+  {
+    "revision": "f3ab421edcfe51dc39be",
+    "url": "/tictactoe/static/js/main.90f5ec8f.chunk.js"
+  },
+  {
+    "revision": "bf9f3814400e9958e663",
+    "url": "/tictactoe/static/js/runtime-main.db19bd63.js"
+  }
+]);
